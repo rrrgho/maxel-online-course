71a18665723ffe82a0e6dd15cec68e9bf85d0224
igho <rianiregho@gmail.com>
2024-01-16 23:00:29 +0700

    Update

--- resources/views/Layouts/header.blade.php
+++ resources/views/Layouts/header.blade.php
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="custom-container container">
             <a class="navbar-brand m-0" href="index.html">
-                <img src="{{asset('assets/images/logo/logo.png')}}" alt="logo" />
+                <img src="{{ asset('assets/images/logo/logo.png') }}" alt="logo" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,38 +13,39 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav navigation">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('/')}}">Home</a>
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('/basic-class')}}">Basic Class</a>
+                        <a class="nav-link" href="{{ url('/basic-class') }}">Basic Class</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('/special-class')}}">Special Class</a>
+                        <a class="nav-link" href="{{ url('/special-class') }}">Special Class</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{url('/private-class')}}">Private Class</a>
+                        <a class="nav-link" href="{{ url('/private-class') }}">Private Class</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#plan"></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#review"></a>
+                        <a href="{{ route('login-user') }}"
+                            class="btn btn-primary d-block d-md-none">Sign in
+                            <i class="fa-solid fa-arrow-right"></i>
+                        </a>
                     </li>
                 </ul>
             </div>
             @if (!Auth::user())
-                <a href="{{route('login-user')}}"
-                {{-- <a href="#" --}}
-                    class="btn btn-primary d-none d-md-block">Sign in
+                <a href="{{ route('login-user') }}" {{-- <a href="#" --}} class="btn btn-primary d-none d-md-block">Sign
+                    in
                     <i class="fa-solid fa-arrow-right"></i>
                 </a>
             @else
-                <a href="{{route('user-dashboard')}}"
-                    class="btn btn-success d-none d-md-block">Your Dashboard
+                <a href="{{ route('user-dashboard') }}" class="btn btn-success d-none d-md-block">Your Dashboard
                     <i class="fa-solid fa-arrow-right"></i>
                 </a>
             @endif
-            
+
         </div>
     </nav>
 </header>
