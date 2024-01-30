6f94d1ab72c6fa4b4420a171d5a7021936998f3f
igho <rianiregho@gmail.com>
2024-01-30 19:32:34 +0700

    Maxelcourse

--- resources/views/Layouts/seo.blade.php
+++ resources/views/Layouts/seo.blade.php
@@ -1,13 +1,13 @@
-<link rel='canonical' href='https://doiscode.com' />
+<link rel='canonical' href='https://maxelcourse.com' />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="theme-color" content="#000000" />
 
-<meta property="og:url" content="https://doiscode.com/" />
-<meta property="og:site_name" content="Doiscode.com">
-<meta property="og:title" content="Digital Business Booster" />
+<meta property="og:url" content="https://maxelcourse.com/" />
+<meta property="og:site_name" content="Maxelcourse.com">
+<meta property="og:title" content="Kelas Digital" />
 <meta property="og:description"
-    content="We help you to Empower Your Business with Expert IT Consulting and New Generation of Digital Solutions." />
+    content="Rasakan keunggulan bergabung dengan Maxel, di mana dengan hanya Rp. 20.000, kamu mendapatkan akses ke ratusan materi terstruktur di Kelas Dasar Digital kami." />
 <meta property="og:image" itemprop="image" content="%PUBLIC_URL%/rel-icon.ico">
 <meta property="og:type" content="website" />
 <meta name="description"
-    content="We help you to Empower Your Business with Expert IT Consulting and New Generation of Digital Solutions." />
+    content="Rasakan keunggulan bergabung dengan Maxel, di mana dengan hanya Rp. 20.000, kamu mendapatkan akses ke ratusan materi terstruktur di Kelas Dasar Digital kami." />
